9f3d66c2534f3b16b4764d1096796e78d68c2499
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-07-07 10:31:15 +0300

    New UPdate

--- resources/views/Student/Exam/Exam_Package.blade.php
+++ resources/views/Student/Exam/Exam_Package.blade.php
@@ -43,6 +43,7 @@ https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
             border-radius: 15px;
             padding: 15px;
             box-shadow: 0px 0px 15px -3px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
         }
 
         .package>span {
@@ -60,14 +61,41 @@ https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
     </div>
     <div class="col-12 allPackages">
         @foreach ($package as $item)
-            <a ref="{{ route('package_checkout', ['id' => $item->id]) }}" class="package">
+            <a href="{{ route('package_checkout', ['id' => $item->id]) }}" class="package">
                 <h3 class="packName texRed">{{ $item->name }}</h3>
                 <span class="packDuration">Duration: {{ $item->duration }} days</span>
-                <span class="packPrice">price: {{ $item->price }} $</span>
+                <span class="packPrice">price: {{ $item->price }}$ </span>
                 <span class="packNum">Number: {{ $item->number }}</span>
             </a>
         @endforeach
     </div>
+
+    {{-- <div class="row g-4">
+        @foreach ($package as $item)
+            <a style="width: 0;" href="{{ route('package_checkout', ['id' => $item->id]) }}">
+                <div class="col-xl-4 col-lg-6 col-md-6 py-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between mb-2">
+                                <h5 class="fw-normal">Price {{ $item->price }}$</h5>
+                            </div>
+                            <div class="d-flex justify-content-between align-items-end">
+                                <div class="role-heading">
+                                    <h4 class="mb-1">{{ $item->name }}</h4>
+                                    <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#addRoleModal"
+                                        class="role-edit-modal"><small>Duration {{ $item->duration }}days</small></a>
+                                    <br />
+                                    <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#addRoleModal"
+                                        class="role-edit-modal"><small>Numbssser {{ $item->number }}</small></a>
+                                </div>
+                                <a href="javascript:void(0);" class="text-muted"><i class="bx bx-copy"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        @endforeach
+    </div> --}}
 @endsection
 
 @include('Student.inc.footer')
