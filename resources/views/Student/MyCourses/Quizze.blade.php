f16e5ae5e75fb65f23fe7b2352feee2093863178
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-09 09:01:26 +0200

    v

--- resources/views/Student/MyCourses/Quizze.blade.php
+++ resources/views/Student/MyCourses/Quizze.blade.php
@@ -651,7 +651,7 @@
                                         <span>Answer:</span>
                                         <div class="input_val">
 
-                                            <input type="number" name='q_grid_ans[]' step="0.001" value="0">
+                                            <input name='q_grid_ans[]' value="0">
                                         </div>
                                     </div>
                                     <div class="section-value">
