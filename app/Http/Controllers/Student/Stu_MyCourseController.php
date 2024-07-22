6275f630b52c2fc2d35cabdb315651ff2ea3a500
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-22 10:17:16 +0200

    v

--- app/Http/Controllers/Student/Stu_MyCourseController.php
+++ app/Http/Controllers/Student/Stu_MyCourseController.php
@@ -79,7 +79,6 @@ class Stu_MyCourseController extends Controller
             }
         }
         $course_id = $id;
-        return $chapters;
 
         return view('Student.MyCourses.Chapters_Working', compact('chapters', 'course_id'));
     }
@@ -447,6 +446,7 @@ class Stu_MyCourseController extends Controller
         ReportVideo::create( $arr );
     }
 
+
     public function api_report_question( Request $req ){
 
         ReportQuestion::create([
@@ -461,5 +461,7 @@ class Stu_MyCourseController extends Controller
             'success' => 'You Make Report success'
         ]);
     }
-  
+
+    
+    
 }
