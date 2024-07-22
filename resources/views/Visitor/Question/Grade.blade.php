9c47179d1bc5baedae03e8afca219ec89766e4ca
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-22 08:59:22 +0200

    v

--- resources/views/Visitor/Question/Grade.blade.php
+++ resources/views/Visitor/Question/Grade.blade.php
@@ -355,7 +355,7 @@
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowfullscreen></iframe> --}}
 
-                                    <iframe scrolling="no" allowfullscreen width="560" height="315"
+                                    <iframe scrolling="no"  height="315"
                                         src="{{ $q_ans->ans_video }}" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowfullscreen></iframe>
