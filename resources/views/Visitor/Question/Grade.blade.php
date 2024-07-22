be1afb031a7d1508cdb2a1ffb4932ee67e07b44a
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-22 09:02:35 +0200

    v

--- resources/views/Visitor/Question/Grade.blade.php
+++ resources/views/Visitor/Question/Grade.blade.php
@@ -355,10 +355,13 @@
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowfullscreen></iframe> --}}
 
-                                    <iframe scrolling="no"  height="315"
-                                        src="{{ $q_ans->ans_video }}" frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowfullscreen></iframe>
+                                <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto;">
+                                    <iframe scrolling="no" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+                                            src="{{ $q_ans->ans_video }}" frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowfullscreen></iframe>
+                                </div>
+                                
 
                                     @if (!empty($q_ans->ans_video))
                                         <div class="list_cont">
