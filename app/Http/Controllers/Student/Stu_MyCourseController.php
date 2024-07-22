2e719273702896919c30a352c7f6f617ec1d3aa2
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-22 15:00:42 +0200

    v

--- app/Http/Controllers/Student/Stu_MyCourseController.php
+++ app/Http/Controllers/Student/Stu_MyCourseController.php
@@ -205,8 +205,8 @@ class Stu_MyCourseController extends Controller
 
     public function quizze_ans(Request $req)
     {
-        return $timer_val;
         $timer_val = json_decode(Cookie::get('timer'));
+        return Cookie::get('timer');
         $quizze_id = json_decode($req->quizze)->id;
         $quizze = quizze::where('id', $quizze_id)
         ->first();
