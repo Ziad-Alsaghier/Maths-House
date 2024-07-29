4f7f324e026b1a5e71f634c1733ca403f5bd29b7
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-29 11:03:37 +0200

    v

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -827,9 +827,6 @@ class ApiController extends Controller
     {
         $user_id = $req->user()->id;
         $chapters = [];
-        return response()->json([
-            'data' => $req->mistakes,
-        ]);
         $mistakes = count(json_decode($req->mistakes)) == 0 ? [] : json_decode($req->mistakes);
         $exam = Exam::where('id', $req->exam_id)
             ->first();
@@ -1017,7 +1014,8 @@ class ApiController extends Controller
             ]);
         }
 
-        $arr = $req->only('f_name', 'l_name', 'email', 'nick_name', 'phone', 'city_id', 'grade');
+        // $arr = $req->only('f_name', 'l_name', 'email', 'nick_name', 'phone', 'city_id', 'grade');
+        $arr = $req->only('f_name', 'l_name', 'email', 'nick_name', 'phone', 'grade');
         $arr['position'] = 'student';
         $arr['state'] = 'hidden';
         $arr['password'] = bcrypt($req->password);
