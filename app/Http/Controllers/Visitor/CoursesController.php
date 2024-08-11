de530a352aa0f85d1f593ce8e19faebad89117d6
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-11 10:50:45 +0200

    v

--- app/Http/Controllers/Visitor/CoursesController.php
+++ app/Http/Controllers/Visitor/CoursesController.php
@@ -152,10 +152,6 @@ class CoursesController extends Controller
             $data = json_decode(Cookie::get('marketing'));
             $chapters_price = floatval(Cookie::get('chapters_price'));
         }
-        else {
-            Cookie::queue('marketing', json_encode($data), 180);
-            Cookie::queue('chapters_price', ($chapters_price), 180);
-        }
          $price_arr = json_encode($price_arr);
         if ( empty(auth()->user()) ) {
             return view('Visitor.Login.login');
