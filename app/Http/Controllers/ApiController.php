22093b4fd87472a7f610f22e759097643c36c61c
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-11 15:27:14 +0200

    v

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -697,9 +697,7 @@ class ApiController extends Controller
             ->with('questions')
             ->with('quizze')
             ->get();
-            foreach ($q_history as $item) {
-                $item->question->q_url = url('images/questions/' . $item->question->q_url);
-            }
+
         return response()->json([
             'question' => $q_history,
             'exam' => $exam_history,
