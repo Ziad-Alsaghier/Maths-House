dd931480377672ae7899dc1939420b65471b348b
Basantsaeed <89366449+Basantsaed@users.noreply.github.com>
2024-09-09 14:12:47 +0300

    fcommit

--- resources/views/Admin/courses/Quizze/Quizze.blade.php
+++ resources/views/Admin/courses/Quizze/Quizze.blade.php
@@ -1669,13 +1669,16 @@
                                         quizze_item.innerHTML = null;
                                         (data.questions).forEach((element,
                                             index) => {
+
+                                                // Check if exam_code exists in element.code, otherwise set it to 'null'
+                                                const examCode = element.code && element.code.exam_code ? element.code.exam_code : 'null';
                                             quizze_item.innerHTML += `<tr class="filterResult">
                                                 <th scope="row" class="idd d-none">${element.id}</th>
                                                 <th>${index + 1}</th>
                                                 <td class="type" id="type">${element.q_type}</td>
                                                 <td class="year" id="year">${element.year}</td>
                                                 <td class="month" id="month">${element.month}</td>
-                                                <td class="code" id="code">${element.code.exam_code}</td>
+                                                <td class="code" id="code">${examCode}</td>
                                                 <td class="section" id="section">${element.section}</td>
                                                 <td class="noNum" id="noNum">${element.q_num}</td>
                                                 <td class="diff" id="diff">${element.difficulty}</td>
