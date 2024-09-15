1ec68e233730d2c5f787f15128c090c73ff4f21b
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-15 16:08:18 +0200

    commit

--- app/Http/Controllers/Student/Stu_MyPackagesController.php
+++ app/Http/Controllers/Student/Stu_MyPackagesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\PaymentPackageOrder;
 use App\Models\PaymentRequest;
+use App\Models\SmallPackage;
 use App\Models\Package;
 
 use Carbon\Carbon;
@@ -31,13 +32,18 @@ class Stu_MyPackagesController extends Controller
             }
         }
 
+        $small_package = SmallPackage::where('user_id', auth()->user()->id)
+        ->get();
+        $s_exams = $small_package->where('module', 'Exam')->sum('number');
+        $s_questions = $small_package->where('module', 'Question')->sum('number');
+        $s_live = $small_package->where('module', 'Live')->sum('number');
         $exam = PaymentPackageOrder::
         leftJoin('payment_requests', 'payment_package_order.payment_request_id', '=', 'payment_requests.id')
         ->leftJoin('packages', 'payment_package_order.package_id', '=', 'packages.id')
         ->where('payment_package_order.state', 1)
         ->where('packages.module', 'Exam')
         ->where('payment_package_order.user_id', auth()->user()->id)
-        ->sum('payment_package_order.number');
+        ->sum('payment_package_order.number') + $s_exams;
         
         $questions = PaymentPackageOrder::
         leftJoin('payment_requests', 'payment_package_order.payment_request_id', '=', 'payment_requests.id')
@@ -45,7 +51,7 @@ class Stu_MyPackagesController extends Controller
         ->where('payment_package_order.state', 1)
         ->where('packages.module', 'Question')
         ->where('payment_package_order.user_id', auth()->user()->id)
-        ->sum('payment_package_order.number');
+        ->sum('payment_package_order.number') + $s_questions;
         
         $live = PaymentPackageOrder::
         leftJoin('payment_requests', 'payment_package_order.payment_request_id', '=', 'payment_requests.id')
@@ -53,7 +59,7 @@ class Stu_MyPackagesController extends Controller
         ->where('payment_package_order.state', 1)
         ->where('packages.module', 'Live')
         ->where('payment_package_order.user_id', auth()->user()->id)
-        ->sum('payment_package_order.number');
+        ->sum('payment_package_order.number') + $s_live;
         
         return view('Student.Package.Package', compact('exam', 'questions', 'live'));
     }
