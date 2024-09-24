ac7639945ac76b8b97ab5e7ad18c07c668b3874f
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-24 12:53:17 +0200

    commit

--- resources/views/ExamQuestionsPDF.blade.php
+++ resources/views/ExamQuestionsPDF.blade.php
@@ -10,18 +10,14 @@
 <body>
     @foreach ( $mistakes as $item)
     <h2>
-        {{$item->question}}
+        {!! $item->question !!}
     </h2> 
     <br />
     @if( !empty($item->q_url) )
     <img src="{{asset('images/questions/' . $item->q_url)}}"  style="width: 100%" />
     @endif
 
-    @if ( ($item->mcq) )
-        Answer : {{$item->mcq[0]->mcq_answers}}
-    @elseif( ($item->g_ans) )
-        Answer : {{$item->g_ans[0]->grid_ans}}
-    @endif
+ 
     <hr />
     @endforeach
 </body>
