f02e7b569951ad24777450bdd719f5697ee5adf2
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-30 14:02:22 +0200

    commit

--- app/Http/Controllers/Student/Stu_PackageController.php
+++ app/Http/Controllers/Student/Stu_PackageController.php
@@ -92,6 +92,12 @@ class Stu_PackageController extends Controller
             return redirect()->back();
         
         }
+        else{ 
+            $arr['payment_method_id'] = $req->payment_method_id;
+            Mail::to('Payment@mathshouse.net')
+            ->queue(new PaymentEmail($req->all(), auth()->user()));
+
+        }
         $p_request = PaymentRequest::create($arr);
         $p_method = isset($p_request->method->payment) ? $p_request->method->payment : 'Wallet';
         $package_data = json_decode(Cookie::get('package'));
@@ -224,11 +230,6 @@ class Stu_PackageController extends Controller
             Cookie::queue(Cookie::forget('q_ans_id'));
         }
         
-        if($req->payment_method_id != 'Wallet'){ 
-            $arr['payment_method_id'] = $req->payment_method_id;
-            Mail::To('Payment@mathshouse.net')
-            ->send(new PaymentEmail($req->all(), auth()->user()));
-        }
         
         return view('Student.Order.Order', compact('package', 'price', 'p_method', 'pages'));
     }
