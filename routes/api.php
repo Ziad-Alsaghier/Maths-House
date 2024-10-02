c0b59c72e233e6632e2a9a732530f3a38defc598
Ahmed <ahmedahmadahmid73@gmail.com>
2024-10-02 14:17:17 +0200

    commit

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,7 @@ Route::get('/api_timer', [V_ExamController::class, 'api_timer'])->name('api_time
 Route::get('/private_req', [Stu_LiveController::class, 'private_req_api'])->name('private_req_api');
 Route::get('/private_req_book', [Stu_LiveController::class, 'private_req_book_api'])->name('private_req_book_api');
 Route::get('/stu_sessions_api', [Stu_LiveController::class, 'stu_sessions_api'])->name('stu_sessions_api');
-Route::get('/lesson_score_sheet', [ScoreController::class, 'lesson_score_sheet'])->name('lesson_score_sheet');
+Route::get('/lesson_score_sheet', [ScoreController::class, 'lesson_score_sheet'])->name('lesson_score_sheet')->middleware(['auth:sanctum']);
 Route::post('/course_score_sheet', [ScoreController::class, 'course_score_sheet'])->name('course_score_sheet');
 
 Route::get('/add_stu_academic', [UserController::class, 'add_stu_academic'])->name('add_stu_academic');
