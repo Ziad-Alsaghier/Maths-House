5f97222f4f54331d45afc77bf15a3e0019b990d1
Ahmed <ahmedahmadahmid73@gmail.com>
2024-10-22 14:13:51 +0200

    commit

--- app/Http/Controllers/Student/Stu_MyCourseController.php
+++ app/Http/Controllers/Student/Stu_MyCourseController.php
@@ -352,6 +352,7 @@ class Stu_MyCourseController extends Controller
             'r_questions' => $right_question,
         ]);
         $quize_id = $stu_quizze->id;
+        return $stu_quizze;
 
         foreach ($mistakes as $item) {
             StudentQuizzeMistake::create([
