6c0f05cae6f582a68b3c78a711f1bac11360e1b1
Ahmed <Wegostationdev@gmail.com>
2025-02-08 11:41:15 +0200

    commit

--- resources/views/Student/Live/MyLiveCourses.blade.php
+++ resources/views/Student/Live/MyLiveCourses.blade.php
@@ -487,9 +487,9 @@ $page_name = 'Lesson';
 
         @endphp
 
-        @if ( \Carbon\Carbon::now()->subDays(7) <= $session->date &&
+        @if ( $session->lesson?->chapter?->course?->id && \Carbon\Carbon::now()->subDays(7) <= $session->date &&
             !in_array($session->lesson?->chapter?->course?->id ?? null,$arr) or
-            (!in_array($session->lesson?->chapter?->course?->id ?? null,$arr) && $session->lesson->getExtraDays() >=
+            $session->lesson?->chapter?->course?->id && (!in_array($session->lesson?->chapter?->course?->id ?? null,$arr) && $session->lesson->getExtraDays() >=
             date('Y-m-d')))
             <div class="col-xl-4 col-lg-6 col-md-6 col-12">
                 <div class="gridarea__wraper">
